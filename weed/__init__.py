e5a87cf854b4f5c8f2e994ec951a294c055ef70b
frol <frolvlad@gmail.com>
2014-04-05 18:49:21 -0700

    removed executable flag from regular files; fixed README to look nicer on github; move version to separate file to safely import it from setup.py

--- weed/__init__.py
+++ weed/__init__.py
@@ -1,16 +1,7 @@
 import weed
 from weed import *
+from .version import VERSION, __version__
 
 __all__ = weed.__all__
 
 __author__ = 'darkdarkfruit'
-
-VERSION = (0, 0, 1)
-
-def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    return version
-
-__version__ = get_version()
